889c83aa245ffc36974b00bbe159f4f6a0e7cd98
AllanWang <me@allanwang.ca>
2016-10-23 19:09:09 -0400

    resize and compress more
    Former-commit-id: 1b2055527f4cbc3c4308ab5b435e6fb1fb5d1731 [formerly 5692d540272aa8a60116d5cf9f51efb7bc24fb1f]
    Former-commit-id: f8a66bc8b59918895107c7f6f2c8554b05e9eab7

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ include("include/header.php"); ?>
 
         </div>
     </div>
-    <div class="parallax dim10"><img src="images/polygon_clouds.jpg" alt="Unsplashed background img 1"></div>
+    <div class="parallax dim10"><img src="images/polygon_clouds.jpg" alt="Polygon Clouds Header"></div>
 </div>
 
 <div class="container">
@@ -36,106 +36,7 @@ include("include/header.php"); ?>
                 <h3><i class="mdi-content-send brown-text"></i></h3>
                 <h4>???</h4>
                 <p class="center-align light">
-                    YZtVEjvkN1uuyDRv76bJ
-                    YDY1IKIy3jVrjAenoyB9
-                    HbdDFek6JxzOc3uC7Jch
-                    ZIBGXvcS5LygCwBxiL7r
-                    z2FWzSSqAf9ozUqS3wnB
-                    iK9WDyU4nOv0zH9rGxT8
-                    SCTXN2wZKLLuAzt5onO4
-                    Km9xlwQp1l65HWbI9Nq4
-                    lVPWjhHj4hKup53DPYaN
-                    UWVtCkRuFoStmITMXp3T
-                    f1OwroZWiFlnixHHQYBQ
-                    xcN5b98itBsBhWYwYpUp
-                    Q0rpbtkwS92eEHvxJ4xw
-                    MQXMYOJTPt1MuYMcOdtf
-                    aBFlFQDMYa53D3rpBOhe
-                    Q7l5CHC2uTAzNRnfAGMU
-                    REN0XZrrfFCkczz5WzY8
-                    UimZfORH1SM7wRvsljMe
-                    sFN9MXU8Kcl8fZZMd8wM
-                    CbVGqVryl89tHePsAJfp
-                    pbbDJQ3tGeCrKOKwtTz8
-                    zB6dqtpR7IUHXuQRWOht
-                    nqJNOMTsN6dewRli1sTA
-                    cB8TUlsscZoT9CnUw1da
-                    XFz0wquXBOWBsCbjkEgt
-                    Km2nGfVXCkkkKgijC48U
-                    XcS0fHKSdTFnIacI0fwZ
-                    omr1OFvYwtI5eMBmy80e
-                    ZorgFYatFmPmwr96Jiht
-                    GnRQZn39oVmq1msVxJyu
-                    ylwWO9PeBu5SDljprk44
-                    LTgEQmMCmOIqot4xfZjO
-                    nHosTUSqaBGXs9w3NRMs
-                    RlnaC170mgfBdeOR03iF
-                    JqhL3Hopbpq1GkeN8ey3
-                    iYkxkcyE5zuamilL3eLu
-                    lKJhNpErCUbeRRtnlPLz
-                    GMSsYxNxRiF4KJM81opD
-                    3mzYT7XnPtL22mlEqZWt
-                    1eaqqIT2TmOAoCrNJCVQ
-                    fNvEAaPK0fyq3TgWzmOc
-                    Jo8zJ6w4N21supO69oV4
-                    bdtMcLsjNG6Nlu3cSzPi
-                    Xp1qc6fZUv2uSdSwRyhl
-                    IULOhxVYzpjlwrFk5kv9
-                    hO0ZIOAx6AW1xTmmst7Q
-                    CEIu9GmCr5AI5OQohW6G
-                    dAXF3FZ6qOw3MnuWdWno
-                    Z26L3SO6OzlSJO6MUbin
-                    skcvnJuf0uqsnxQhyTtr
-                    g3dKj7yPjzLhapMWXi0L
-                    XKOAqLxcw80zvBG6PXHL
-                    QgzdcrBRaNn8oGznhx9Y
-                    XNLUWs3RBhZ3FewaScpE
-                    zrbMw5WvQkIonSzQOUBB
-                    7goSpWtY3FfUQAeDaGNF
-                    O9nPGCm9RLsEKOfuE6nZ
-                    l9ZhUPsnvmJO6vC1oEoL
-                    OSbQ03zFWM2J4aqUbEZx
-                    Y6xc5id17n7iVF2f0k0x
-                    ERlAFHfFJJOeDeYdnE2S
-                    er2hBHq8ZJOFlMqhitPm
-                    fElSQg6IJJwPTHHvGHpg
-                    8DWqOOmdBdbVW3m28DCg
-                    tS2F7NH3K2UgwhDMQ2uF
-                    obV8mdvQ1koh4c6eChVr
-                    vGFzJUVyZhTwbixkpqLi
-                    qkTzEcSIAR9LW9aLrAYJ
-                    wyC7AvCQJXUWckQp8OXn
-                    3aQwqC3oJI5SRWe99AKx
-                    juxO85LTc73HOLbOuvNj
-                    tpH7om1uJojGselPRuAc
-                    0OPgIvaFhjgz57zPuf6m
-                    ODthhv0OlsXmTIsFrNTd
-                    1SoM2cFigwWDDU8febJJ
-                    3dI7ObDoTkwenNk4ioA8
-                    hd2aReFyeAMOBJRk7PfB
-                    EK3Oj1HWr6GegxtPbZ34
-                    Gv3QA7Mvob7JGzxe2Lj5
-                    SWgVU8Es75g5yRFKbEZR
-                    RUwJw9dIqsKjCGYeCEM8
-                    goItRRlqTsymFfEZQtJ0
-                    REiOwW6CHCqH3KQHHnXZ
-                    aR1aDzBpeoPvAC9eqpiL
-                    BiwMhNSmpvqhQ3ia9BSR
-                    jjjK0jpfL6B449aDYKby
-                    vOrQHAuGSr2yQEt2fZUj
-                    FK5ES9AtqeCjv7JYKEkO
-                    sDQcSWHIRJaFUR6VI66S
-                    oXUZh82RnEr0MmmD5I5R
-                    DCmzsmz3XehX1J1MlqKn
-                    3mBuoDVrTQbMCFqXDiuv
-                    VgmFDBGLVpwvp1Io8s7l
-                    eC5D8U8PtuEZwEan57oX
-                    Admct9YTXU640XGykP43
-                    qY1hmquvdzkIkO3ELCV0
-                    cy7z8XYvdJvBSS03l2Eo
-                    UhOsmM7dvnT196WwzMe2
-                    cH8yZ1jzYmDiPNAWI3n8
-                    eNmTLKFT97qNFoFtvt5L
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     <br><br>
                     Psst you can press an item in the nav bar
                 </p>
