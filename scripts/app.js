8ff9ff28c97c6c716ca1a18239b7bc8742a8680c
AllanWang <me@allanwang.ca>
2017-01-04 00:14:47 -0500

    bower css

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,23 @@
-var app = angular.module('frameApp', ['angularCss']);
+angular.module('frameApp', ['ui.router', 'angularCSS'])
 
-app.constant('include_url', 'http://allanwang.ca/include/');
+    .constant('include_url', 'http://allanwang.ca/include/')
+
+    .config(function ($stateProvider) {
+        $stateProvider.state('dev', {
+            url: "/dev",
+            // controller : 'TemCtrl',
+            templateUrl : 'view.html',
+            resolve: {
+                $title: function() {
+                    return 'Projects';
+                // },
+                // endpoints: function($rootScope, tepidServer) {
+                //     if (!$rootScope.session || !$rootScope.session.user) return false;
+                //     return tepidServer.getEndpoints();
+                }
+            // },
+            // data: {
+            //     'rolesAllowed': ['ctfer', 'elder']
+            }
+        });
+    });
