98337832ee3f6f730c201f5f8832631b4cff9666
Allan Wang <me@allanwang.ca>
2017-01-04 10:49:38 -0500

    nav html

--- scripts/app.js
+++ scripts/app.js
@@ -4,20 +4,24 @@ angular.module('frameApp', ['ui.router', 'angularCSS'])
 
     .config(function ($stateProvider) {
         $stateProvider.state('dev', {
-            url: "/dev",
-            // controller : 'TemCtrl',
-            templateUrl : 'view.html',
-            resolve: {
-                $title: function() {
-                    return 'Projects';
-                // },
-                // endpoints: function($rootScope, tepidServer) {
-                //     if (!$rootScope.session || !$rootScope.session.user) return false;
-                //     return tepidServer.getEndpoints();
+            views: {
+                'main': {
+                    url: "/dev",
+                    // controller : 'TemCtrl',
+                    templateUrl: 'view.html',
+                    resolve: {
+                        $title: function () {
+                            return 'Projects';
+                            // },
+                            // endpoints: function($rootScope, tepidServer) {
+                            //     if (!$rootScope.session || !$rootScope.session.user) return false;
+                            //     return tepidServer.getEndpoints();
+                        }
+                        // },
+                        // data: {
+                        //     'rolesAllowed': ['ctfer', 'elder']
+                    }
                 }
-            // },
-            // data: {
-            //     'rolesAllowed': ['ctfer', 'elder']
             }
         });
     });
