48a50beb8f5181d48c07138790d3eddf2922704b
AllanWang <me@allanwang.ca>
2017-01-16 12:45:44 -0500

    amino

--- notes/mcgill/bioc212/amino-acids/data.js
+++ notes/mcgill/bioc212/amino-acids/data.js
@@ -1,6 +1,12 @@
 const FULL_NAME = 0, CODE_TRIPLE = 1, CODE_SINGLE = 2;
 
 /*
+ * Links
+ * http://wbiomed.curtin.edu.au/biochem/tutorials/aaquiz/index.html
+ * https://upload.wikimedia.org/wikipedia/commons/5/51/Molecular_structures_of_the_21_proteinogenic_amino_acids.svg
+ */
+
+/*
  * E electrical, P positive, N negative
  * P polar uncharged
  * H hydrophobic
@@ -8,105 +14,106 @@ const FULL_NAME = 0, CODE_TRIPLE = 1, CODE_SINGLE = 2;
  */
 const SC_E_P = 0, SC_E_N = 1, SC_P = 2, SC_H = 3, SC_S = 4;
 
-exports.aminoAcids = [
+export default aminoAcids = [
     {
         "name": ["alanine", "ala", "A"],
-        "side_chain": SC_H,
-        "codons": ["GCA", "GCC", "GCG", "GCT"]
-    },
-    {
+        "sideChain": SC_H,
+        "codons": ["GCA", "GCC", "GCG", "GCT"],
+        "molFormula": "C3H7NO2"
+    }, {
         "name": ["arginine", "arg", "R"],
-        "side_chain": SC_E_P,
-        "codons": ["AGA", "AGG", "CGA", "CGC", "CGG", "CGT"]
-    },
-    {
+        "sideChain": SC_E_P,
+        "codons": ["AGA", "AGG", "CGA", "CGC", "CGG", "CGT"],
+        "molFormula": "C6H14N4O2"
+    }, {
         "name": ["asparagine", "asn", "N"],
-        "side_chain": SC_P,
-        "codons": ["AAC", "AAT"]
-    },
-    {
+        "sideChain": SC_P,
+        "codons": ["AAC", "AAT"],
+        "molFormula": "C4H8N2O3"
+    }, {
         "name": ["aspartic acid", "asp", "D"],
-        "side_chain": SC_E_N,
-        "codons": ["GAC", "GAT"]
-    },
-    {
+        "sideChain": SC_E_N,
+        "codons": ["GAC", "GAT"],
+        "molFormula": "C4H7NO4"
+    }, {
         "name": ["cysteine", "cys", "C"],
-        "side_chain": SC_S,
-        "codons": ["TGC", "TGT"]
-    },
-    {
+        "sideChain": SC_S,
+        "codons": ["TGC", "TGT"],
+        "molFormula": "C3H7NO2S"
+    }, {
         "name": ["glutamine", "gln", "Q"],
-        "side_chain": SC_P,
-        "codons": ["CAA", "CAG"]
-    },
-    {
+        "sideChain": SC_P,
+        "codons": ["CAA", "CAG"],
+        "molFormula": "C5H10N2O3"
+    }, {
         "name": ["glutamic acid", "glu", "E"],
-        "side_chain": SC_E_N,
-        "codons": ["GAA", "GAG"]
-    },
-    {
+        "sideChain": SC_E_N,
+        "codons": ["GAA", "GAG"],
+        "molFormula": "C5H9NO4"
+    }, {
         "name": ["glycine", "gly", "G"],
-        "side_chain": SC_S,
-        "codons": ["GGA", "GGC", "GGG", "GGT"]
-    },
-    {
+        "sideChain": SC_S,
+        "codons": ["GGA", "GGC", "GGG", "GGT"],
+        "molFormula": "C2H5NO2"
+    }, {
         "name": ["histidine", "his", "H"],
-        "side_chain": SC_E_P,
-        "codons": ["CAC", "CAT"]
-    },
-    {
+        "sideChain": SC_E_P,
+        "codons": ["CAC", "CAT"],
+        "molFormula": "C6H9N3O2"
+    }, {
         "name": ["isoleucine", "ile", "I"],
-        "side_chain": SC_H,
-        "codons": ["ATA", "ATC", "ATT"]
-    },
-    {
+        "sideChain": SC_H,
+        "codons": ["ATA", "ATC", "ATT"],
+        "molFormula": "C6H13NO2"
+    }, {
         "name": ["leucine", "leu", "L"],
-        "side_chain": SC_H,
-        "codons": ["CTA", "CTC", "CTG", "CTT", "TTA", "TTG"]
-    },
-    {
+        "sideChain": SC_H,
+        "codons": ["CTA", "CTC", "CTG", "CTT", "TTA", "TTG"],
+        "molFormula": "C6H13NO2"
+    }, {
         "name": ["lysine", "lys", "K"],
-        "side_chain": SC_E_P,
-        "codons": ["AAA", "AAG"]
-    },
-    {
+        "sideChain": SC_E_P,
+        "codons": ["AAA", "AAG"],
+        "molFormula": "C6H14N2O2"
+    }, {
         "name": ["methionine", "met", "M"],
-        "side_chain": SC_H,
-        "codons": ["ATG"]
-    },
-    {
+        "sideChain": SC_H,
+        "codons": ["ATG"],
+        "molFormula": "C5H11NO2S"
+    }, {
         "name": ["phenylalanine", "phe", "F"],
-        "side_chain": SC_H,
-        "codons": ["TTC", "TTT"]
-    },
-    {
+        "sideChain": SC_H,
+        "codons": ["TTC", "TTT"],
+        "molFormula": "C9H11NO2"
+    }, {
         "name": ["proline", "pro", "P"],
-        "side_chain": SC_S,
-        "codons": ["CCA", "CCC", "CCG", "CCT"]
-    },
-    {
+        "sideChain": SC_S,
+        "codons": ["CCA", "CCC", "CCG", "CCT"],
+        "molFormula": "C5H9NO2"
+    }, {
         "name": ["serine", "ser", "S"],
-        "side_chain": SC_P,
-        "codons": ["AGC", "AGT", "TCA", "TCC", "TCG", "TCT"]
-    },
-    {
+        "sideChain": SC_P,
+        "codons": ["AGC", "AGT", "TCA", "TCC", "TCG", "TCT"],
+        "molFormula": "C3H7NO3"
+    }, {
         "name": ["threonine", "thr", "T"],
-        "side_chain": SC_P,
-        "codons": ["ACA", "ACC", "ACG", "ACT"]
-    },
-    {
+        "sideChain": SC_P,
+        "codons": ["ACA", "ACC", "ACG", "ACT"],
+        "molFormula": "C4H9NO3"
+    }, {
         "name": ["tryptophan", "trp", "W"],
-        "side_chain": SC_H,
-        "codons": ["TGG"]
-    },
-    {
+        "sideChain": SC_H,
+        "codons": ["TGG"],
+        "molFormula": "C11H12N2O2"
+    }, {
         "name": ["tyrosine", "tyr", "Y"],
-        "side_chain": SC_H,
-        "codons": ["TAC", "TAT"]
-    },
-    {
+        "sideChain": SC_H,
+        "codons": ["TAC", "TAT"],
+        "molFormula": "C9H11NO3"
+    }, {
         "name": ["valine", "val", "V"],
-        "side_chain": SC_H,
-        "codons": ["GTA", "GTC", "GTG", "GTT"]
+        "sideChain": SC_H,
+        "codons": ["GTA", "GTC", "GTG", "GTT"],
+        "molFormula": "C5H11NO2"
     }
 ];
--- notes/mcgill/bioc212/amino-acids/index.php
+++ notes/mcgill/bioc212/amino-acids/index.php
@@ -20,88 +20,14 @@ phpHeader(); ?>
         <h2>Biochemistry 212</h2>
         <div class="row light">
             <div id="notes-container" class="col s12 m9 l10">
-                <div id="lecture-1" class="section scrollspy">
-                    <h5>Lecture 1 – 2017/01/04</h5>
-                    <?php
-                    dynamicBullets(
-                        "#Endocrine – long distance",
-                        "#Paracrine – short distance, ie neighbouring cell",
-                        "#Autocrine – self, eg growth factor",
-                        "Endocrine signaling",
-                        "-Hormone secretion into blood by endocrine gland &rarr; transported to distant site",
-                        "Communication by hormones (or neurohormones)",
-                        "-Synthesis by endocrine cells",
-                        "-Release by endocrine cells",
-                        "-Transport by blood stream",
-                        "-Detection by specific receptor protein on target cells",
-                        "-Change in cellular metabolism by hormone-receptor interactions",
-                        "-Removal of hormone – terminates cellular response",
-                        "Hypothalamic-Pituitary Signaling",
-                        "Hormones have different types of structures",
-                        "-#Peptides | Proteins &rarr; Glyco | Poly",
-                        "-Glycoproteins/polypeptides &rarr; must have gene that encodes that hormone",
-                        "-Steroids/Amines do not have genes that encode them",
-                        "--#Do have genes that encode enzymes for synthesis of those molecules",
-                        "-Ionic calcium – exists a calcium sensing receptor",
-                        "Synthesis of protein hormones",
-                        "-#Transcription of rna from gene",
-                        "-#Protein produced is inserted into endoplasmic reticulum",
-                        "-#Proteins are transformed from N terminus to C terminus",
-                        "--#Preprosequence – additional sequence on N terminus ",
-                        "---#Not in biologically active hormone",
-                        "---#Present in primary translation product of that hormone",
-                        "---#Represent molecular barcodes that direct protein to secretory system",
-                        "---#Cleaved off during secretory process (no longer present in mature hormone)",
-                        "---#For glycoproteins, oligosaccharide component is added during trip through endoplasmic reticulum in golgi to primary translation product",
-                        "Structures of some steroid hormones",
-                        "-Cortisol/Aldosterone#{ – similar structures, different physiology}",
-                        "-Testosterone#{ – has very important methyl group that stops it from becoming estradiol}",
-                        "-Estradiol – has an aromatic ring",
-                        "--Essential for bone physiology",
-                        "--Women get it from ovaries",
-                        "--Men get it from local conversion of testosterone",
-                        "---#Has enzyme that burns the methyl group off",
-                        "Thyroid hormones",
-                        "-Not steroids, derived from tyrosine, contain iodine",
-                        "Properties of hormone receptors",
-                        "-Specificity – recognition of single hormone or hormone family",
-                        "-Affinity – binding hormone at its physiological concentration",
-                        "--#If concentrations are hugely different, system would be always on/off, not physiological",
-                        "-#Should show saturability and have a measurable biological response",
-                        "Receptor regulation",
-                        "-Upregulation – increase activity in response to hormone or synthesis",
-                        "-Downregulated – decreasing activity or synthesis",
-                        "Hormones can exert effects on target cells",
-                        "-Direct effects on function at cell membrane",
-                        "-Intracellular effects mediated by second messenger systems",
-                        "-Intracellular effects mediated by genomic or nuclear action",
-                        "--#Eg steroid hormone – hormone binds to receptor in cytoplasm &rarr; transcription in nucleus &rarr; protein synthesis &rarr; altered functional response"
-                    );
-                    ?>
+                <div id="summary">
+                    <div id="structures"></div>
                 </div>
             </div>
             <div class="col hide-on-small-only m3 l2">
-                <div class="pinned vertical-center">
-                    <div class="switch">
-                        <label>
-                            Collapse
-                            <input id="note-toggle" type="checkbox" onchange="toggleNoteView(this)" checked>
-                            <span class="lever"></span>
-                            Expand
-                        </label>
-                    </div>
-                    <ul class="section table-of-contents">
-                        <li><a href="#lecture-1">Lecture 1</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div id="keypanel" class="modal bottom-sheet">
-                <div class="modal-content">
-                    <?php
-                    keywordPanel();
-                    ?>
-
-                </div>
+                <?php
+                tableOfContents();
+                ?>
             </div>
         </div>
     </div>
