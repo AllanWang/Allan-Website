3994bb5c800e7d090ed2239871e27b5175e345c2
AllanWang <me@allanwang.ca>
2017-02-05 00:30:50 -0500

    working dev snap

--- include/js/home.ts
+++ include/js/home.ts
@@ -1,28 +1,30 @@
-var themeColor = '#0097A7';
-var lineAttr = {
+import Paper = Snap.Paper;
+const themeColor = '#0097A7';
+const lineAttr = {
     stroke: themeColor,
     strokeWidth: 15,
     strokeLinecap: "round",
     opacity: 0
 };
-var svg;
-var developer = [];
-var developerData = [
+let svg: Snap.Paper;
+let developer: Snap.Element[] = [];
+const developerData = [
     [20, 100, 43, 31],
     [20, 100, 43, 169],
     [280, 100, 257, 31],
     [280, 100, 257, 169]
 ];
-var developerSpeed = 200;
+const developerSpeed = 200;
+
 $(function () {
     svg = Snap('#aw-svg');
-    var bItems = $("#b-row").find(".btn-flat");
-    var cContainer = $('#w-content-container');
+    const bItems = $("#b-row").find(".btn-flat");
+    const cContainer = $('#w-content-container');
     bItems.each(function (index) {
         $(this).click(function () {
             bItems.not($(this)).removeClass('selected');
             $(this).addClass('selected');
-            var id = $(this).attr('id').split('-')[1]; //buttons have b-x notation; get x
+            const id = $(this).attr('id').split('-')[1]; //buttons have b-x notation; get x
             cContainer.removeClass().addClass(id);
             hideAll();
             switch (id) {
@@ -38,37 +40,34 @@ $(function () {
                     return;
             }
         });
-    });
+    })
 });
+
 function hideAll() {
     if (developer.length != 0) {
-        var _loop_1 = function(path) {
+        for (let path of developer) {
             path.animate({
                 opacity: 0
             }, developerSpeed, mina.linear, function () {
                 path.remove();
             });
-        };
-        for (var _i = 0, developer_1 = developer; _i < developer_1.length; _i++) {
-            var path = developer_1[_i];
-            _loop_1(path);
         }
         developer = [];
     }
 }
+
+
 function showDeveloper() {
-    if (developer.length != 0)
-        return;
+    if (developer.length != 0) return;
     console.log('showdev');
-    for (var _i = 0, developerData_1 = developerData; _i < developerData_1.length; _i++) {
-        var data = developerData_1[_i];
-        var line = svg.polyline("M" + data[0] + "," + data[1] + " L" + data[0] + "," + data[1]);
+    for (const data of developerData) {
+        let line = svg.polyline(`M${data[0]},${data[1]} L${data[0]},${data[1]}`);
         line.attr(lineAttr);
         line.animate({
-            d: "M" + data[0] + "," + data[1] + " L" + data[2] + "," + data[3],
+            d: `M${data[0]},${data[1]} L${data[2]},${data[3]}`,
             opacity: 1
         }, developerSpeed);
         developer.push(line);
     }
 }
-//# sourceMappingURL=home.js.map
+
--- index.php
+++ index.php
@@ -13,50 +13,57 @@ phpHeader(); ?>
 <?php phpNav(); ?>
 
 <main>
-    <div id="index-banner" class="parallax-container">
-        <div class="section no-pad-bot">
-            <div class="container">
-                <br>
-                <br>
-                <h1 class="header center white-text text-lighten-2 pad-top-20">Home Page</h1>
-                <div class="row center">
-                    <h5 class="header col s12 light white-text">//TODO</h5>
-                </div>
-                <!-- <div class="row center">
-          <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light black lighten-1">Get Started</a>
-        </div> -->
-                <br>
-                <br>
-
-            </div>
-        </div>
-        <div class="parallax blur-darken"><img src="images/polygon_clouds.jpg" alt="Polygon Clouds Header"></div>
-    </div>
 
     <div class="container">
         <div class="section">
 
-            <div class="row">
-                <div class="col m3">
-                    <div style="height: 200px"></div>
-<!--                    <img src="images/aw-material.svg">-->
+            <div id="w-logo-container" class="center">
+                <svg id="aw-svg" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+                    <path id="w-1"
+                          d="M90,55 L120,145"
+                          stroke="#000"
+                          stroke-width="15"
+                          stroke-linecap="round"></path>
+                    <path id="w-4"
+                          d="M180,145 L210,55"
+                          stroke="#000"
+                          stroke-width="15"
+                          stroke-linecap="round"></path>
+                    <path id="w-2"
+                          d="M120,145 L150,55"
+                          stroke="<?php echo $theme_color?>"
+                          stroke-width="15"
+                          stroke-linecap="round"></path>
+                    <path id="w-3"
+                          d="M150,55 L180,145"
+                          stroke="<?php echo $theme_color?>"
+                          stroke-width="15"
+                          stroke-linecap="round"></path>
+                    <path id="w-drop"
+                          d="M150,100 L149,103 A1,1,0,0,0,151,103"
+                          fill="<?php echo $theme_color?>"></path>
+                    <animate xlink:href="#w-drop"
+                             attributeName="d"
+                             attributeType="XML"
+                             to="M150,100 L143,121 A7,7,0,0,0,157,121"
+                             dur="0.3s"
+                             begin="2.2s"
+                             fill="freeze"></animate>
+                </svg>
+            </div>
+            <div id="b-row" class="row center-align">
+                <div class="col s4">
+                    <a id="b-themer" class="waves-effect btn-flat">Themer</a>
                 </div>
-                <div class="col m9 center-align">
-                    <h6 id="testt" class="vertical-center"></h6>
-                    <div class='content'>
-                        <div class='visible'>
-                            <div class="il">
-                                Hello
-                            </div>
-                            <ul class="ir">
-                                <li>world !</li>
-                                <li>Bob !</li>
-                                <li>users !</li>
-                                <li>everybody !</li>
-                            </ul>
-                        </div>
-                    </div>
+                <div class="col s4">
+                    <a id="b-developer" class="waves-effect btn-flat">Developer</a>
                 </div>
+                <div class="col s4">
+                    <a id="b-student" class="waves-effect btn-flat selected">Student</a>
+                </div>
+            </div>
+            <div id="w-content-container">
+
             </div>
 
         </div>
@@ -64,8 +71,10 @@ phpHeader(); ?>
 </main>
 <?php
 phpFooter();
-//js('home');
 ?>
+<script type="text/javascript" src="/bower_components/Snap.svg/dist/snap.svg-min.js"></script>
+<script type="text/javascript" src="/include/js/home.js"></script>
+
 </body>
 
 </html>
