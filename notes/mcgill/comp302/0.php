184a547b39fbe44ff023925e1487371c41b29978
Allan Wang <me@allanwang.ca>
2017-09-14 13:09:49 -0400

    comp302/lecture-2

--- notes/mcgill/comp302/0.php
+++ notes/mcgill/comp302/0.php
@@ -25,7 +25,7 @@ $subHeader = 'Lectures 0 – 5';
         <div class="row light">
             <div id="notes-container" class="col s12 m9 l10">
                 <?php
-                lectureSection(0, '2017/09/12',
+                lectureSection(1, '2017/09/12',
                     "5 hw (25%), midterm (10%), final (65%)",
                     "Office hours – McConnell Eng. 107N – Tue, Thu 11:30am – 12:30pm; Thu 4:00pm – 5:30pm",
                     "Goals",
@@ -35,6 +35,20 @@ $subHeader = 'Lectures 0 – 5';
                     "-Grammar & parsing, operation semantics & interpreters, type checking, polymorphism, subtyping",
                     "-Expose students to different way of thinking about problems",
                     "Thorough notes for this class will be up on MyCourses each week"
+                );
+
+                lectureSection(2, '2017/09/14',
+                    "Statically typed programs approximate runtime behaviour & analyze programs before executing them; help find & fix bugs before testing",
+                    "Functional languages are primarily expressed via functions They are first-class (you may pass & return functions)",
+                    "-Pure functional languages (eg Haskell) don't allow any modifications for variables, and do not provide exceptions",
+                    "-Not pure functions (eg OCaml) allows for other paradigms as well",
+                    "Call-By-Value – execute when instruction is reached",
+                    "Lazy – execute once it is needed to compute other instructions",
+                    "Keywords & Operators",
+                    "-+, –, *, / are for (int, int) -> int functions",
+                    "-+., –., *., /. are for (float, float) -> float functions",
+                    "in – keyword for using a variable locally",
+                    "-When  the same variable is recreated, it overshadows the previous one. It does not update the previous binding"
                 );
 
                 pagination();
