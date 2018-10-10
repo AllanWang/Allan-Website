05cf16df53933fd306804373e21de32664dc8d94
Allan Wang <me@allanwang.ca>
2018-10-09 23:59:26 -0400

    Update about section

--- about/index.php
+++ about/index.php
@@ -25,13 +25,15 @@ phpNav(); ?>
             <div class="row">
                 <div class="col s12 m9 l10">
                     <h3 class="header center">About Allan Wang</h3>
-                    <span>//TODO ¯\_(ツ)_/¯ I am currently in my second year at <a
-                                href="http://mcgill.ca/">McGill University</a> studying <s>Pharmacology and Computer Science</s> Software Engineering</span>
+                    <span>//TODO ¯\_(ツ)_/¯ I am currently in my last year at <a
+                                href="http://mcgill.ca/">McGill University</a> studying <s>Pharmacology and Computer Science</s> Software Engineering</span>.
+                    I interned at Google as a UX Engineer in the summer of 2018, and will be going back as a FTE in
+                    2019.
 
-                    <div id="dev">
-                        <img class="dev-icon big" src="dev/java.svg">
-                        <img class="dev-icon medium" src="dev/node-js.svg">
-                    </div>
+                    <!--                    <div id="dev">-->
+                    <!--                        <img class="dev-icon big" src="dev/java.svg">-->
+                    <!--                        <img class="dev-icon medium" src="dev/node-js.svg">-->
+                    <!--                    </div>-->
 
                 </div>
                 <div class="col hide-on-small-only m3 l2">
